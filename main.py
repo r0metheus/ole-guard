652985ec169c53e94a5950833180a98bc48a0dd0
Francesco Romeo <francesco_romeo@me.com>
2021-12-14 19:42:49 +0100

    redo from scratch

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python3
 
+from oletools.olevba import VBA_Parser, TYPE_OLE, TYPE_OpenXML, TYPE_Word2003_XML, TYPE_MHTML
+import hashlib
 import subprocess
-import scraped_pb2
 import sys
+import os
+import magic
+import json
 
-encoding = 'utf-8'
-def hashes(file, scraped):
-	scraped.md5 = (subprocess.run(["md5sum", file], capture_output=True).stdout.split()[0]).decode(encoding)
-	scraped.sha1 = (subprocess.run(["sha1sum", file], capture_output=True).stdout.split()[0]).decode(encoding)
-	scraped.sha256 = (subprocess.run(["sha256sum", file], capture_output=True).stdout.split()[0]).decode(encoding)
-	scraped.sha512 = (subprocess.run(["sha512sum", file], capture_output=True).stdout.split()[0]).decode(encoding)
+encoding = 'latin-1'
+
+def md5(filename):
+    hash_md5 = hashlib.md5()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def sha1(filename):
+    hash_sha1 = hashlib.sha1()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha1.update(chunk)
+    return hash_sha1.hexdigest()
+
+def sha256(filename):
+    hash_sha256 = hashlib.sha256()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
+
+def sha512(filename):
+    hash_sha512 = hashlib.sha512()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha512.update(chunk)
+    return hash_sha512.hexdigest()
 
 def generic(file, scraped):
-	scraped.mime = (subprocess.run(["file", "--mime-type", "-b", file], capture_output=True).stdout).decode(encoding)
-	scraped.filetype = (subprocess.run(["file", file], capture_output=True).stdout).decode(encoding).strip(file)[2:]
 	m = (subprocess.run(["xxd", "-l", "8",  file], capture_output=True).stdout).decode(encoding).split(':')[1].split()[:-1]
 	magic = ""
 	for c in m:
@@ -35,29 +60,39 @@ def strings(file, exp, scraped):
 def oledump(file, scraped):
 	scraped.oledump = (subprocess.run(["oledump.py", "-d", "--decompress", file], capture_output=True).stdout).decode(encoding)
 
-def olevba(file, scraped):
-	scraped.olevba = (subprocess.run(["olevba", file, "--decode"], capture_output=True).stdout).decode(encoding)
-
-	min_i = max_i = 0
-	for i, s in enumerate(scraped.olevba.split()):
-		if '+-' in s:
-			if min_i == 0:
-				min_i = i
-			else:
-				max_i = i
-	ovba = scraped.olevba.split()[min_i:max_i+1]
-
 def main():
-	filename = sys.argv[1]
-	scraped = scraped_pb2.Scraped()
-	hashes(filename, scraped)
-	generic(filename, scraped)
-	strings(filename, 'C:\\', scraped)
-	oledump(filename, scraped)
-	olevba(filename, scraped)
+	directory = sys.argv[1]
+	structure = {}
 
-	f = open(filename+'_scraped', "wb")
-	f.write(scraped.SerializeToString())
+	for filename in os.listdir(directory):
+		file = directory + os.path.sep + filename
+		vbaparser = VBA_Parser(file)
+		
+		structure[filename] = {}
+		structure[filename]['md5'] = md5(file)
+		structure[filename]['sha1'] = sha1(file)
+		structure[filename]['sha256'] = sha256(file)
+		structure[filename]['sha512'] = sha512(file)
+		structure[filename]['mime'] = magic.from_file(file, mime=True)
+		
+		with open(file, 'rb') as f:
+			structure[filename]['magic'] = f.read(8).hex()
+
+		vbaparser.analyze_macros(show_decoded_strings=False, deobfuscate=False)
+		results = vbaparser.analysis_results
+
+		sus = []
+		for kw_type, keyword, description in results:
+			if 'to obf' in description:
+				sus.append(keyword)
+
+		structure[filename]['obfuscation_methods'] = list(set(sus))
+		structure[filename]['macros_num'] = vbaparser.nb_macros()
+
+		with open(directory+'_results', 'a') as f:
+			json.dump(structure[filename], f)
+			f.write(os.linesep)
+
 	f.close()
 
 if __name__ == '__main__':
